1f1d32cc3e6638c26d12d6aa576ab933859cbf3a
Denylson Melo <denylson.melo@ifpi.edu.br>
2016-12-30 19:24:52 -0300

    resolvendo problema 1006

--- src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1006.java
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1006.java
@@ -8,14 +8,15 @@ import java.io.InputStreamReader;
  *
  * @author Denylson Melo
  */
-public class URI_1005 {
+public class URI_1006 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        double valor1, valor2;
+        double valor1, valor2, valor3;
         valor1 = Double.parseDouble(br.readLine());
         valor2 = Double.parseDouble(br.readLine());
+        valor3 = Double.parseDouble(br.readLine());
 
-        System.out.println(String.format("MEDIA = %.5f", (valor1 * 3.5 + valor2 * 7.5) / 11));
+        System.out.println(String.format("MEDIA = %.1f", (valor1 * 2 + valor2 * 3 + valor3 * 5) / 10));
     }
 }

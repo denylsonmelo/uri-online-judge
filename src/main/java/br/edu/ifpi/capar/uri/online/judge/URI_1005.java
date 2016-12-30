069e3c443b9f3b9cfbe1b6061aeb0c2716d02f14
Denylson Melo <denylson.melo@ifpi.edu.br>
2016-12-30 19:20:29 -0300

    resolvendo problema 1005

---
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1005.java
@@ -0,0 +1,21 @@
+package br.edu.ifpi.capar.uri.online.judge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Denylson Melo
+ */
+public class URI_1005 {
+     public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double valor1, valor2;
+        valor1 = Double.parseDouble(br.readLine());
+        valor2 = Double.parseDouble(br.readLine());
+
+        System.out.println(String.format("MEDIA = %.5f", (valor1*3.5+valor2*7.5)/11));
+    }
+}

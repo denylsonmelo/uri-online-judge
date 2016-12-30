a563399d29e8b3a53707593b145de485aa43ca03
Denylson Melo <denylson.melo@ifpi.edu.br>
2016-12-30 19:04:43 -0300

    resolvendo problema 1003

---
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1003.java
@@ -0,0 +1,21 @@
+package br.edu.ifpi.capar.uri.online.judge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Denylson Melo
+ */
+public class URI_1003 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int valor1, valor2;
+        valor1 = Integer.parseInt(br.readLine());
+        valor2 = Integer.parseInt(br.readLine());
+
+        System.out.println("SOMA = " + (valor1 + valor2));
+    }
+}

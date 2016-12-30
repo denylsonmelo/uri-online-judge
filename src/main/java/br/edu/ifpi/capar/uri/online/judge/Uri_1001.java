19a6f06054f3da71505f357e056ab5ca366ccdf5
Denylson Melo <denylson.melo@ifpi.edu.br>
2016-12-30 18:25:13 -0300

    resolvendo problema 1001

---
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/Uri_1001.java
@@ -0,0 +1,21 @@
+package br.edu.ifpi.capar.uri.online.judge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Denylson Melo
+ */
+public class Uri_1001 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int valor1, valor2;
+        valor1 = Integer.parseInt(br.readLine());
+        valor2 = Integer.parseInt(br.readLine());
+
+        System.out.println("X = " + (valor1 + valor2));
+    }
+}

fefdcbc2f6bc4eda1c30d55fb7537aed82c6302f
Denylson Melo <denylson.melo@ifpi.edu.br>
2017-05-04 18:06:06 -0300

    problema e testes para o 1060

---
+++ src/test/java/br/edu/ifpi/capar/uri/online/judge/URI_1060Test.java
@@ -0,0 +1,18 @@
+package br.edu.ifpi.capar.uri.online.judge;
+
+import br.edu.ifpi.capar.uri.online.judge.base.URITestBaseImpl;
+import org.junit.Test;
+
+/**
+ *
+ * @author Denylson Melo
+ */
+public class URI_1060Test extends URITestBaseImpl {
+
+    @Test
+    @Override
+    public void deveValidarExemplosDeEntradaESaidaDoSite() {
+        paraAClasse(this.getClass());
+        afirmeQueComAsEntradas("7", "-5", "6", "-3.4", "4.6", "12").temosAsSaidas("4 valores positivos");
+    }
+}

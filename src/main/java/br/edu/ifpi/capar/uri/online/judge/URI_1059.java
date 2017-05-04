5c2fd0513b083a1b7315f86fab2cb11d19fd6d81
Denylson Melo <denylson.melo@ifpi.edu.br>
2017-05-04 17:54:21 -0300

    problema e testes para o 1059

---
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1059.java
@@ -0,0 +1,16 @@
+package br.edu.ifpi.capar.uri.online.judge;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Denylson Melo
+ */
+public class URI_1059 {
+
+    public static void main(String[] args) throws IOException {
+        for (int par = 2; par <= 100; par=par+2) {
+            System.out.println(par);
+        }
+    }
+}
---
+++ src/test/java/br/edu/ifpi/capar/uri/online/judge/URI_1059Test.java
@@ -0,0 +1,67 @@
+package br.edu.ifpi.capar.uri.online.judge;
+
+import br.edu.ifpi.capar.uri.online.judge.base.URITestBaseImpl;
+import org.junit.Test;
+
+/**
+ *
+ * @author Denylson Melo
+ */
+public class URI_1059Test extends URITestBaseImpl {
+
+    @Test
+    @Override
+    public void deveValidarExemplosDeEntradaESaidaDoSite() {
+        paraAClasse(this.getClass());
+        afirmeQueComAsEntradas("").temosAsSaidas("2\n"
+                + "4\n"
+                + "6\n"
+                + "8\n"
+                + "10\n"
+                + "12\n"
+                + "14\n"
+                + "16\n"
+                + "18\n"
+                + "20\n"
+                + "22\n"
+                + "24\n"
+                + "26\n"
+                + "28\n"
+                + "30\n"
+                + "32\n"
+                + "34\n"
+                + "36\n"
+                + "38\n"
+                + "40\n"
+                + "42\n"
+                + "44\n"
+                + "46\n"
+                + "48\n"
+                + "50\n"
+                + "52\n"
+                + "54\n"
+                + "56\n"
+                + "58\n"
+                + "60\n"
+                + "62\n"
+                + "64\n"
+                + "66\n"
+                + "68\n"
+                + "70\n"
+                + "72\n"
+                + "74\n"
+                + "76\n"
+                + "78\n"
+                + "80\n"
+                + "82\n"
+                + "84\n"
+                + "86\n"
+                + "88\n"
+                + "90\n"
+                + "92\n"
+                + "94\n"
+                + "96\n"
+                + "98\n"
+                + "100");
+    }
+}

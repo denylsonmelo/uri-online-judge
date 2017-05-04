e553e371472188489b1f6c34cd5cc83a5cb2aff0
Denylson Melo <denylson.melo@ifpi.edu.br>
2017-05-03 23:02:31 -0300

    correcao do problema 1021

--- src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1021.java
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1021.java
@@ -12,8 +12,8 @@ public class URI_1021 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int notas100, notas50, notas20, notas10, notas5, notas2, moedas1real, moedas50, moedas25, moedas10, moedas5, moedas1;
-        double valor = Double.parseDouble(br.readLine());
+        int notas100, notas50, notas20, notas10, notas5, notas2, moedas1real, moedas50, moedas25, moedas10, moedas5;
+        double moedas1, valor = Double.parseDouble(br.readLine());
 
         notas100 = (int) (valor / 100);
         notas50 = (int) (valor % 100 / 50);
@@ -26,7 +26,7 @@ public class URI_1021 {
         moedas25 = (int) (valor % 100 % 50 % 20 % 10 % 5 % 2 % 1 % 0.5 / 0.25);
         moedas10 = (int) (valor % 100 % 50 % 20 % 10 % 5 % 2 % 1 % 0.5 % 0.25 / 0.1);
         moedas5 = (int) (valor % 100 % 50 % 20 % 10 % 5 % 2 % 1 % 0.5 % 0.25 % 0.1 / 0.05);
-        moedas1 = (int) (valor % 100 % 50 % 20 % 10 % 5 % 2 % 1 % 0.5 % 0.25 % 0.1 % 0.05 / 0.01);
+        moedas1 = (double) (valor % 100 % 50 % 20 % 10 % 5 % 2 % 1 % 0.5 % 0.25 % 0.1 % 0.05 / 0.01);
 
         System.out.println("NOTAS:\n"
                 + notas100 + " nota(s) de R$ 100.00\n"
@@ -40,7 +40,7 @@ public class URI_1021 {
                 + moedas50 + " moeda(s) de R$ 0.50\n"
                 + moedas25 + " moeda(s) de R$ 0.25\n"
                 + moedas10 + " moeda(s) de R$ 0.10\n"
-                + moedas5 + " moeda(s) de R$ 0.05\n"
-                + moedas1 + " moeda(s) de R$ 0.01");
+                + moedas5 + " moeda(s) de R$ 0.05");
+        System.out.printf("%.0f moeda(s) de R$ 0.01\n", moedas1);
     }
 }

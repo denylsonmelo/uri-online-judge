c227b17562a6530287021bb06378084d2188e879
Denylson Melo <denylson.melo@ifpi.edu.br>
2017-05-04 19:04:45 -0300

    problema e testes para o 1067

--- src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1067.java
+++ src/main/java/br/edu/ifpi/capar/uri/online/judge/URI_1067.java
@@ -1,16 +1,23 @@
 package br.edu.ifpi.capar.uri.online.judge;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
  * @author Denylson Melo
  */
-public class URI_1059 {
+public class URI_1067 {
 
     public static void main(String[] args) throws IOException {
-        for (int par = 2; par <= 100; par=par+2) {
-            System.out.println(par);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int valorLido = Integer.parseInt(br.readLine());
+
+        for (int numero = 0; numero <= valorLido; numero++) {
+            if (numero % 2 != 0) {
+                System.out.println(numero);
+            }
         }
     }
 }
